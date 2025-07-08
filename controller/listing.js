c9f7965b3b16705b8b7bbaabac70059165785bab
Hrugwed <hrugwedzagade123@gmail.com>
2025-07-08 09:25:17 +0530

    A little change in deleteListings

--- controller/listing.js
+++ controller/listing.js
@@ -15,7 +15,7 @@ module.exports.index = async (req, res) => {
             listings = await Listing.find({});
         }
     }
- 
+
     else if (q && q.length > 0) {
         const regex = new RegExp('^' + q, 'i');
         listings = await Listing.find({
@@ -31,7 +31,7 @@ module.exports.index = async (req, res) => {
             listings = await Listing.find({});
         }
     }
- 
+
     else {
         listings = await Listing.find({});
     }
@@ -136,11 +136,10 @@ module.exports.updateListing = async (req, res) => {
 
 
 // Delete
+
 module.exports.deleteListing = async (req, res) => {
-    module.exports.deleteListing = async (req, res) => {
-    let { id } = req.params; 
+    let { id } = req.params;
     await Listing.findByIdAndDelete(id);
-    req.flash('success', 'Listing deleted successfully!'); 
+    req.flash('success', 'Listing deleted successfully!');
     res.redirect('/listings');
-};
 };
